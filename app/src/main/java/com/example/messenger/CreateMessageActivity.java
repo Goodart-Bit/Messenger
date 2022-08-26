1aa0742297ea2ca78a50ab683962984a40a4733f
Goodart <juandlh416@gmail.com>
2022-08-25 19:53:19 -0500

    created messenger app

---
+++ app/src/main/java/com/example/messenger/CreateMessageActivity.java
@@ -0,0 +1,26 @@
+package com.example.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createmessageactivity);
+    }
+
+    public void sendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(this,ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE,messageText);
+        startActivity(intent);
+    }
+}
